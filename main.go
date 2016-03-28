3c82e29b51ee412cfa37935e728d90aca564e5f9
mjamesruggiero <mjamesruggiero@gmail.com>
2016-03-27 20:34:56 -0700

    Ready for Heroku

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"github.com/russross/blackfriday"
+	"os"
+
+	"net/http"
+)
 
 func main() {
-	http.ListenAndServe(":8081", http.FileServer(http.Dir(".")))
+	port := os.GetEnv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":"+port, nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
 }
